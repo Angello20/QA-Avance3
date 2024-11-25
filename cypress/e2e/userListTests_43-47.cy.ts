e3f5e85f508ac4e3de2f5536e3a442ec230bfff2
JoseAndresVargas <andyxd76@gmail.com>
2024-11-24 22:56:50 -0600

    Modificación pruebas 39-42

--- cypress/e2e/userListTests_43-47.cy.ts
+++ cypress/e2e/userListTests_43-47.cy.ts
@@ -15,7 +15,7 @@ describe('Pruebas de pagina "User List"', () => {
         cy.get('.lock-icon').click();
 
         // Inicia sesión como administrador
-        cy.get('input[placeholder="Email"]').clear().type('admin@gmail.com'); 
+        cy.get('input[placeholder="Email"]').clear().type('admin@gmail.com');
         cy.get('input[placeholder="Password"]').clear().type('123456');
         cy.get('button[type="submit"]').click();
 
@@ -40,83 +40,87 @@ describe('Pruebas de pagina "User List"', () => {
         // Espera a que se complete la solicitud de exportación
         cy.wait(4000);
 
-        const downloadsFolder = Cypress.config('downloadsFolder'); 
+        const downloadsFolder = Cypress.config('downloadsFolder');
         cy.readFile(`${downloadsFolder}/Export.xlsx`).should('exist');
 
     });
     it('Caso de Prueba No. 40: Añadir un nuevo usuario en "User List" llenando todos los campos', () => {
         // Navega a la pestaña "Settings"
         cy.contains('Settings').click();
-    
+
         // Navega a la pestaña "User List"
         cy.contains('User List').click();
         cy.url().should('include', '/UserList');
-    
+
         // Simula un clic en el botón para añadir un nuevo usuario
         cy.contains('Add').click();
         cy.url().should('include', '/Users/UserForm?action=create');
 
-    
+
         // Espera explícitamente a que los campos estén visibles
-        cy.get('#Email').should('be.visible').type('example@example.com');
-        cy.get('#Password]').should('be.visible').type('password123');
-        cy.get('#ConfirmPassword').should('be.visible').type('password123');
-        cy.get('#SelectedCompanyId').select('Default Company, LLC.');
-    
+        cy.get('#UserForm_Email').should('be.visible').type('example@gmail.com');
+        cy.get('#UserForm_Password').should('be.visible').type('password123');
+        cy.get('#UserForm_ConfirmPassword').should('be.visible').type('password123');
+        cy.get('span[aria-owns="UserForm_SelectedCompanyId_options"]').click();
+        cy.wait(500);
+        cy.contains('.e-popup .e-list-item', 'Default Company, LLC.').should('be.visible').click();
+
         // Envía el formulario
-        cy.get('button[type="submit"]').click();
-    
+        cy.get('#btnSubmit').click();
+
         // Verifica que se muestre una notificación de éxito o una confirmación de creación de usuario
         cy.contains('Success create new data.').should('be.visible');
     });
     it('Caso de Prueba No. 41: Añadir un nuevo usuario en "User List" sin llenar el campo "Email"', () => {
         // Navega a la pestaña "Settings"
         cy.contains('Settings').click();
-    
+
         // Navega a la pestaña "User List"
         cy.contains('User List').click();
         cy.url().should('include', '/UserList');
-    
+
         // Simula un clic en el botón para añadir un nuevo usuario
         cy.contains('Add').click();
         cy.url().should('include', '/Users/UserForm?action=create');
-    
+
         // Espera explícitamente a que los campos estén visibles
-        cy.get('#Password').should('be.visible').type('password123');
-        cy.get('#ConfirmPassword').should('be.visible').type('password123');
-        cy.get('#SelectedCompanyId').select('Default Company, LLC.');
-    
+        cy.get('#UserForm_Password').should('be.visible').type('password123');
+        cy.get('#UserForm_ConfirmPassword').should('be.visible').type('password123');
+        cy.get('span[aria-owns="UserForm_SelectedCompanyId_options"]').click();
+        cy.wait(500);
+        cy.contains('.e-popup .e-list-item', 'Default Company, LLC.').should('be.visible').click();
+
         // Envía el formulario sin llenar el campo "Email"
-        cy.get('button[type="submit"]').click();
-    
+        cy.get('#btnSubmit').click();
+
         // Verifica que se muestre una notificación de error para el campo "Email"
         cy.contains('The Email field is required.').should('be.visible');
     });
     it('Caso de Prueba No. 42: Añadir un nuevo usuario en "User List" sin seleccionar la compañía', () => {
         // Navega a la pestaña "Settings"
         cy.contains('Settings').click();
-    
+
         // Navega a la pestaña "User List"
         cy.contains('User List').click();
         cy.url().should('include', '/UserList');
-    
+
         // Simula un clic en el botón para añadir un nuevo usuario
         cy.contains('Add').click();
         cy.url().should('include', '/Users/UserForm?action=create');
-    
+
         // Espera explícitamente a que los campos estén visibles
-        cy.get('#Email').should('be.visible').type('example@example.com');
-        cy.get('#Password').should('be.visible').type('password123');
-        cy.get('#ConfirmPassword').should('be.visible').type('password123');
-    
+        cy.get('#UserForm_Email').should('be.visible').type('example@example.com');
+        cy.get('#UserForm_Password').should('be.visible').type('password123');
+        cy.get('#UserForm_ConfirmPassword').should('be.visible').type('password123');
+
         // Deja el campo "Selected Company" vacío (no se selecciona ninguna opción)
-        cy.get('#SelectedCompanyId').should('be.visible'); // Asegúrate de que esté visible pero no seleccionas nada
-    
+        cy.get('span[aria-owns="UserForm_SelectedCompanyId_options"]').should('be.visible'); // Asegúrate de que esté visible pero no seleccionas nada
+
         // Envía el formulario sin seleccionar la compañía
-        cy.get('button[type="submit"]').click();
-    
+        cy.get('#btnSubmit').click();
+
         // Verifica que se muestre una notificación de error para el campo "Selected Company"
         cy.contains('The Selected Company field is required.').should('be.visible');
     });
-    
+
 });
